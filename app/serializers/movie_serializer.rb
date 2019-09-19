11b8b3ef98befc4ad29039c77ee2c193c1d91cce
wmonges <setwildo31@gmail.com>
2019-09-19 16:30:47 -0400

    include relationshipt in json response

--- app/serializers/movie_serializer.rb
+++ app/serializers/movie_serializer.rb
@@ -7,4 +7,8 @@ class MovieSerializer
   attributes :title
 
   attribute :release_year, &:release_year_in_roman
+
+  Role.roles.each do |role|
+    has_many role.to_s.pluralize.to_sym
+  end
 end
--- app/serializers/person_serializer.rb
+++ app/serializers/person_serializer.rb
@@ -4,4 +4,10 @@ class PersonSerializer
 
   attributes :id, :last_name, :first_name, :gender
   has_many :aliases
+
+  Role.roles.each do |role|
+    attribute "movies_as_#{role}" do |person|
+      person.movies.where(movie_person_roles: { role: role })
+    end
+  end
 end
